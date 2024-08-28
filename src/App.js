38ba02b75c89b197a4409e26f188d65f107ba93d
Shubhraj-Shubh <shubhrajput19194@gmail.com>
2024-08-28 17:22:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <div className="fullContainer">
+      <div className="navbar">
+        <Navbar/>
+        </div>
+      <div className="bigbox">
+     <div className="boxpuzzle">
+<div className="heading">
+  <h1>15 puzzle</h1>
+</div>
+     </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+
+const navbar = () => {
+  return (
+    <div className="navbar">
+   Navbar
+    </div>
+  )
+}
+
+export default navbar
